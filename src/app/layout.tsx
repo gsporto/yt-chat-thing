f6c48604b28885797f33ea4485f8a8cc38aed9d9
Gabriel Porto <gabriel_porto@outlook.com>
2024-07-15 01:02:27 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "YoutubeChatThing",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
