45277a90272fd6c3aa6fe99b90db0685fd41629f
Gabriel Porto <gabriel_porto@outlook.com>
2024-08-07 21:24:08 -0300

    empty temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,20 @@ export default async function Home({
 }: {
   searchParams: { channel_id: string };
 }) {
-  const liveId = await fetchIframe(channel_id);
-
-  const { hostname } = new URL(BASE_URL);
-
   return (
-    <iframe
-      src={`https://www.youtube.com/live_chat?v=${liveId}&embed_domain=${hostname}`}
-      style={{ width: "100%", height: "100%", border: "none" }}
-    />
-  );
+    <div>
+      
+    </div>
+  )
+
+  // const liveId = await fetchIframe(channel_id);
+
+  // const { hostname } = new URL(BASE_URL);
+
+  // return (
+  //   <iframe
+  //     src={`https://www.youtube.com/live_chat?v=${liveId}&embed_domain=${hostname}`}
+  //     style={{ width: "100%", height: "100%", border: "none" }}
+  //   />
+  // );
 }
